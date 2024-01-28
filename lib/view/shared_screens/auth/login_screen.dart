e2d9f8a205bb39c59fefb80462d5ce735de00b4a
RNheru <Roshdy@zunko.app>
2024-01-28 19:54:00 +0200

    Edit login add calendar request

--- lib/view/shared_screens/auth/login_screen.dart
+++ lib/view/shared_screens/auth/login_screen.dart
@@ -23,116 +23,126 @@ class LoginScreen extends StatelessWidget {
           padding: const EdgeInsets.only(bottom: 24),
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: GetBuilder<AuthController>(builder: (logic) {
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Center(
-                    child: Padding(
-                      padding: const EdgeInsets.only(top: 20),
-                      child: SvgPicture.asset(
-                        "assets/icons/logo.svg",
-                        width: 97,
-                        height: 65,
-                      ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 20),
+                    child: SvgPicture.asset(
+                      "assets/icons/logo.svg",
+                      width: 97,
+                      height: 65,
                     ),
                   ),
-                  Center(
-                    child: Padding(
-                      padding: const EdgeInsets.only(top: 28),
-                      child: Column(
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                "${"Welcome".tr} ",
-                                style: AppFonts.header.copyWith(fontSize: 20),
-                              ),
-                              const SizedBox(
-                                width: 2,
-                              ),
-                              Image.asset(
-                                "assets/images/welcome.png",
-                                width: 22,
-                                height: 22,
-                              )
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            "Enter your account here".tr,
-                            style: AppFonts.medium.copyWith(
-                                fontWeight: FontWeight.w400,
-                                color: AppColors.darkBlue500Base),
-                          )
-                        ],
-                      ),
+                ),
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 28),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "${"Welcome".tr} ",
+                              style: AppFonts.header.copyWith(fontSize: 20),
+                            ),
+                            const SizedBox(
+                              width: 2,
+                            ),
+                            Image.asset(
+                              "assets/images/welcome.png",
+                              width: 22,
+                              height: 22,
+                            )
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "Enter your account here".tr,
+                          style: AppFonts.medium.copyWith(
+                              fontWeight: FontWeight.w400,
+                              color: AppColors.darkBlue500Base),
+                        )
+                      ],
                     ),
                   ),
-                  const SizedBox(
-                    height: 24,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Email".tr,
-                        style: AppFonts.style12Urb.copyWith(
-                            fontWeight: FontWeight.w600,
-                            color: AppColors.darkBlue500Base),
-                      ),
-                      const SizedBox(
-                        height: 8,
-                      ),
-                      EmailTextField(
-                          controller: loginController.emailController,
-                          onSubmit: (String value) {},
-                          onChange: (String value) {
-                            loginController.checkEmailValidation();
-                          },
-                          hint: 'Email'.tr,
-                          label: '',
-                          fieldValidation: loginController.emailValidation),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 24,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Password".tr,
-                        style: AppFonts.style12Urb.copyWith(
-                            fontWeight: FontWeight.w600,
-                            color: AppColors.darkBlue500Base),
-                      ),
-                      const SizedBox(
-                        height: 8,
-                      ),
-                      PasswordTextField(
-                          controller: loginController.passwordController,
-                          fieldValidation: loginController.passwordValidate,
-                          validateText: 'Password should be more than 6 digits',
-                          onSubmit: (val) {},
-                          label: '',
-                          hintText: 'Password'.tr,
-                          onChange: (val) {
-                            loginController.checkPasswordValidation();
-                          }),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 12,
-                  ),
-                  const ForgotPassword(),
-                  const SizedBox(
-                    height: 24,
-                  ),
-                  DefaultButton(
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Email".tr,
+                      style: AppFonts.style12Urb.copyWith(
+                          fontWeight: FontWeight.w600,
+                          color: AppColors.darkBlue500Base),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    GetBuilder<AuthController>(
+                      builder: (_) {
+                        return EmailTextField(
+                            controller: loginController.emailController,
+                            onSubmit: (String value) {},
+                            onChange: (String value) {
+                              loginController.checkEmailValidation();
+                            },
+                            hint: 'Email'.tr,
+                            label: '',
+                            fieldValidation: loginController.emailValidation);
+                      },
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Password".tr,
+                      style: AppFonts.style12Urb.copyWith(
+                          fontWeight: FontWeight.w600,
+                          color: AppColors.darkBlue500Base),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    GetBuilder<AuthController>(
+                      builder: (_) {
+                        return PasswordTextField(
+                            controller: loginController.passwordController,
+                            fieldValidation: loginController.passwordValidate,
+                            validateText:
+                                'Password should be more than 6 digits',
+                            onSubmit: (val) {},
+                            label: '',
+                            hintText: 'Password'.tr,
+                            onChange: (val) {
+                              loginController.checkPasswordValidation();
+                            });
+                      },
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                const ForgotPassword(),
+                const SizedBox(
+                  height: 24,
+                ),
+                Obx(() {
+                  return DefaultButton(
+                    loading: loginController.isLoginLoading.value,
                     function: () {
                       loginController.signInAPI(context);
                     },
@@ -140,10 +150,10 @@ class LoginScreen extends StatelessWidget {
                     borderRadius: 8,
                     color: AppColors.logo,
                     height: 48,
-                  ),
-                ],
-              );
-            }),
+                  );
+                }),
+              ],
+            ),
           ),
         ),
       ),
